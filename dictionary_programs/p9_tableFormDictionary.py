067a7703ab6d39ffa504823b38b32ca4652a25d4
Nagashree3007 <crnagashree8@gmail.com>
2024-07-13 20:54:16 +0530

    [Nagashree] Add:Printing the given dictionary key value pairs in table form.

---
+++ dictionary_programs/p9_tableFormDictionary.py
@@ -0,0 +1,27 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-12
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-12  
+@Title : Printing the given dictionary key value pairs in table form.
+'''
+
+
+def table_dictionary(dict1):
+    """
+    Description: Function to printing the given dictionary key value pairs in the form of table view to user 
+    Parameter: 
+        dict1: function taking given dictionary as a parameters into function
+    Return:  
+    """
+
+    print("KEY\t\tValue")
+    print("---------------------------")
+    for key,value in dict1.items():
+        print(f"{key}: \t\t {value}")
+    print("---------------------------")
+
+
+dict1 = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+table_dictionary(dict1)
+

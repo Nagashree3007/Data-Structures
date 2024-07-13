0a12f6a93f5a5eba55ce5bd352ae9e830a7a1bd8
Nagashree3007 <crnagashree8@gmail.com>
2024-07-13 14:11:13 +0530

    [Nagashree] Add:generate lists and tuple for given sequence.

---
+++ basic_programs/p2_sequence.py
@@ -0,0 +1,16 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-12
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-12
+@Title : generate lists and tuple for given sequence.
+'''
+
+#take input of the values for converting it in to list
+values=list(map(str,input("enter the values").split(",")))
+print(values)
+
+#take input of the values for converting it in to tuple
+
+values=tuple(map(str,input("enter the values").split(",")))
+print(values)

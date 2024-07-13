0b462fb9659c5c27bd16a0abcc8d74d79d33c7ee
Nagashree3007 <crnagashree8@gmail.com>
2024-07-13 21:24:14 +0530

    [Nagashree] Add:Program to create frozenset.

---
+++ sets_programs/p11_frozenset.py
@@ -0,0 +1,35 @@
+"""
+
+@Author: Nagashree C R
+@Date: 2024-07-13
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-13 
+@Title : Program to create an frozenset.
+
+"""
+
+def frozen_set():
+
+    """
+
+    description:
+        This function is used create an frozenset.
+    
+    parameters:
+        None
+        
+    return:
+        none
+
+    """
+    frozen_set = frozenset([1, 2, 3, 4, 5])
+    print(frozen_set)
+          
+
+def main():
+
+    frozen_set()
+
+
+if __name__ == "__main__":
+    main()

020f2ae1ebe9cad23aea5e0dab0c2f06a537c7b7
Nagashree3007 <crnagashree8@gmail.com>
2024-07-13 14:45:35 +0530

    [Nagashree] Add:Creating and iterating array elements.

---
+++ arrray_programs/p1_creatingArray.py
@@ -0,0 +1,27 @@
+'''
+@Author: Nagashree C R
+@Date: 2024-07-12
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-12
+@Title : Creating and iterating array elements
+'''
+
+
+"""
+    Description: Function to iterates the elements present in the Array and prints based on indexs
+    Parameter: list of elements
+    Return:elements and their index
+    """
+    
+
+array_of_integers = [10, 20, 30, 40, 50]
+print("Elements of the array based on index:")
+for i in range(len(array_of_integers)):
+   print(f"Element at index {i} = {array_of_integers[i]}")
+
+
+
+
+
+
+

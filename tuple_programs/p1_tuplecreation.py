d62a02f08f50200c7cdabae147974a8e65f2b5db
Nagashree3007 <crnagashree8@gmail.com>
2024-07-14 09:04:59 +0530

    [Nagashree] Add:Program to create a tuple

---
+++ tuple_programs/p1_tuplecreation.py
@@ -0,0 +1,34 @@
+"""
+
+@Author: Nagashree C R
+@Date: 2024-07-13
+@Last Modified by: Nagashree C R
+@Last Modified: 2024-07-13
+@Title : Program to create a tuple.
+
+"""
+
+def create_tuple(tuple_of_numbers):
+
+    """
+
+    description:
+        This function is used to create tuple.
+    
+    parameters:
+        tuple_of_numbers(tuple)) : numbers in tuple we are printing.
+        
+    return:
+        none
+
+    """
+
+    print(tuple_of_numbers)
+
+def main():
+    tuple_of_numbers = (1, 2, 3, 4)
+    create_tuple(tuple_of_numbers)
+
+
+if __name__ == "__main__":
+    main()

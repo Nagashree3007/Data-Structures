347d87f092f2630dd9eee447cdfd6c74a1ec5ddf
Nagashree3007 <crnagashree8@gmail.com>
2024-07-24 15:05:02 +0530

    [Nagashree] Updated:Counting the Occurrence of given element.

--- arrray_programs/p3_countOccuranceofele.py
+++ arrray_programs/p3_countOccuranceofele.py
@@ -1,10 +1,20 @@
 '''
+
 @Author: Nagashree C R
 @Date: 2024-07-12
 @Last Modified by: Nagashree C R
 @Last Modified: 2024-07-12
 @Title : Counting the Occurrence of given element
+
 '''
+
+import os
+
+import loggingfile
+
+current_scriptname=os.path.basename(__file__)
+
+logger=loggingfile.logg(current_scriptname)
 
 
 def count_occurrences(arr, element):
@@ -23,9 +33,11 @@ def count_occurrences(arr, element):
             count += 1
     return count
 
+def main():
+    arr = [1, 2, 3, 4, 2, 2, 3, 1, 1]
+    element = 2
+    logger.debug(f"Number of occurrences of {element} in the array: {count_occurrences(arr, element)} times")
 
-arr = [1, 2, 3, 4, 2, 2, 3, 1, 1]
-element = 2
-print(f"Number of occurrences of {element} in the array: {count_occurrences(arr, element)} times")
-
-
+if __name__ == "__main__":
+    main()
+    

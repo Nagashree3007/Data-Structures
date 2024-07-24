df2eb73688e60185d461c8afb71d52ab2c39d694
Nagashree3007 <crnagashree8@gmail.com>
2024-07-24 15:04:36 +0530

    [Nagashree] Updated: Reverse the order of items in the Array.

--- arrray_programs/p2_reversingElements.py
+++ arrray_programs/p2_reversingElements.py
@@ -1,22 +1,29 @@
 '''
+
 @Author: Nagashree C R
 @Date: 2024-07-12
 @Last Modified by: Nagashree C R
 @Last Modified: 2024-07-12 
 @Title : Reverse the order of items in the Array
+
+
 '''
 
 
-"""
-    Description: Function  to iterate array elements in backward direction from last index position to frist and prints that array
-    Parameter: list of elements
-    Return:reversed array
-    """    
-array_of_integers = [10, 20, 30, 40, 50]
-print("Original array:--->", array_of_integers)
+import os
+
+import loggingfile
+
+current_scriptname=os.path.basename(__file__)
+
+logger=loggingfile.logg(current_scriptname)
+
+array_of_integers = [10, 20, 60 , 40, 50]
+
+logger.info("Original array:--->{}".format(array_of_integers))
+
 #for reversing the elements
 reversed_array = array_of_integers[::-1]
 array2_integers =reversed_array
-print("Reversed array:--->", array2_integers )
 
-
+logger.info("Reversed array:---> {}".format( array2_integers ))
